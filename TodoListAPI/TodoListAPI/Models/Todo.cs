cd0bc36cb820552a66b746b1e752b5c2cc8e6d42
Pedro Seibel <64672223+pedroseibel@users.noreply.github.com>
2022-03-11 10:38:16 -0300

    TodoListAPI with CRUD and SQL Server

---
+++ TodoListAPI/TodoListAPI/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TodoListAPI.Models
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
